93da55c74549fb96453a79324e2ee971f9612f76
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-01-17 14:52:46 +0100

    feat: remove unicorn server in main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-import uvicorn
-from src.Server import Server
-from src.cloud_services.aws_service import AwsService
+from app.Server import Server
+from app.cloud_services.aws_service import AwsService
 
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
@@ -9,11 +8,5 @@ AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_DESTINATION = os.getenv("AWS_DESTINATION", "")
 
-def main():
-    server = Server(AwsService(AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME, AWS_REGION, AWS_DESTINATION))
-    server.start()
-
-    uvicorn.run(server.app, host="127.0.0.1", port=8000)
-
-if __name__ == "__main__":
-    main()
+server = Server(AwsService(AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME, AWS_REGION, AWS_DESTINATION))
+server.start()
