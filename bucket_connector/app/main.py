133a7e857c8d2f39bb32227e8c8984a6a482f6bf
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-01-20 14:33:59 +0100

    feat: add comment on router to add env port and server url

--- bucket_connector/app/main.py
+++ bucket_connector/app/main.py
@@ -3,7 +3,7 @@ from app.Server import Server
 
 def main():
     server = Server().start()
-
+    # TODO: replace with env
     uvicorn.run(server.app, host="127.0.0.1", port=8000)
 
 if __name__ == "__main__":
