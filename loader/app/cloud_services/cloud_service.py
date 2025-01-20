6d73555f8a64d63f8dbaa6bd832e8f70a0880cb9
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-01-20 14:32:49 +0100

    feat: remove unnecessary files

--- loader/app/cloud_services/cloud_service.py
+++
@@ -1,9 +0,0 @@
-class CloudService:
-    def connect(self) -> None:
-        pass
-
-    def disconnect(self) -> None:
-        pass
-
-    def load(self, source : any) -> None:
-        pass
--- loader/app/exceptions/authentication_failed_exception.py
+++
@@ -1,3 +0,0 @@
-class AuthenticationFailedException(Exception):
-    def __init__(self, message : str):
-        self.message = message
