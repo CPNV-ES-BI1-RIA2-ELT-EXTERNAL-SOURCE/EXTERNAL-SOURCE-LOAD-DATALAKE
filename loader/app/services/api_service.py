1f4b5526049b4393ccf3ca17ffa7fd3552bbd46b
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-01-23 18:11:13 +0100

    fix: raise when sub service not found

--- loader/app/services/api_service.py
+++ loader/app/services/api_service.py
@@ -11,17 +11,15 @@ def api_call(method, url, headers=None, data=None, params=None):
     :param params: Paramètres de requête (GET uniquement, optionnel)
     :return: Réponse de l'API sous forme d'objet JSON ou texte brut
     """
-    try:
-        if method.upper() == 'GET':
-            response = requests.get(url, headers=headers, params=params)
-        elif method.upper() == 'POST':
-            response = requests.post(url, headers=headers, json=data)
-        else:
-            raise ValueError("La méthode doit être 'GET' ou 'POST'")
+    if method.upper() == 'GET':
+        response = requests.get(url, headers=headers, params=params)
+    elif method.upper() == 'POST':
+        response = requests.post(url, headers=headers, json=data)
+    else:
+        raise ValueError("La méthode doit être 'GET' ou 'POST'")
 
-        response.raise_for_status()
+    response.raise_for_status()
 
-        return response.text
+    return response.text
 
-    except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
+
